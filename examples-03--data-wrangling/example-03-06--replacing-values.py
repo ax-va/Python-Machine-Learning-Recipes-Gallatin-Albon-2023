a2c2fc1a4a18f61a7a75cfa6cf1fcff1f47ada12
ax-va <vasiliev.informatik@t-online.de>
2024-04-01 21:10:33 +0200

    update

--- examples-03--data-wrangling/example-03-06--replacing-values.py
+++ examples-03--data-wrangling/example-03-06--replacing-values.py
@@ -31,8 +31,16 @@ df.replace(1, "One").head(3)
 # 2  Allison, Mr Hudson Joshua Creighton    1st  30.0    male        0       0
 
 # Replace values with regex
-df.replace(r".st", "First", regex=True).head(3)
-#                                   Name PClass   Age     Sex  Survived  SexCode
-# 0         Allen, Miss Elisabeth Walton  First  29.0  female         1        1
-# 1          Allison, Miss Helen Loraine  First   2.0  female         0        1
-# 2  Allison, Mr Hudson Joshua Creighton  First  30.0    male         0        0
+df.replace(r".st", "First", regex=True)
+#                                                Name PClass    Age     Sex  Survived  SexCode
+# 0                      Allen, Miss Elisabeth Walton  First  29.00  female         1        1
+# 1                       Allison, Miss Helen Loraine  First   2.00  female         0        1
+# 2               Allison, Mr Hudson Joshua Creighton  First  30.00    male         0        0
+# 3     Allison, Mrs Hudson JC (Bessie Waldo Daniels)  First  25.00  female         0        1
+# 4                   Allison, MFirster Hudson Trevor  First   0.92    male         1        0
+# ...                                             ...    ...    ...     ...       ...      ...
+# 1308                             Zakarian, Mr Artun    3rd  27.00    male         0        0
+# 1309                         Zakarian, Mr Maprieder    3rd  26.00    male         0        0
+# 1310                               Zenni, Mr Philip    3rd  22.00    male         0        0
+# 1311                               Lievens, Mr Rene    3rd  24.00    male         0        0
+# 1312                                 Zimmerman, Leo    3rd  29.00    male         0        0
