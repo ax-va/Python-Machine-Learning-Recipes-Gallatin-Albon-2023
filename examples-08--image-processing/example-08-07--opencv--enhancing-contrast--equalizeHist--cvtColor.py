ed9779ddc3763f4737796827b492327d1140d4ad
ax-va <vasiliev.informatik@t-online.de>
2024-06-06 21:12:11 +0200

    update

---
+++ examples-08--image-processing/example-08-07--opencv--enhancing-contrast--equalizeHist--cvtColor.py
@@ -0,0 +1,44 @@
+"""
+Increase the contrast between pixels in an image.
+->
+Histogram equalization is a tool for standing out image objects and shapes.
+That is able to make objects of interest more distinguishable from other objects or backgrounds.
+
+For images in grayscale:
+- equalizeHist
+
+For color images:
+- Convert to YUV (Y = luma, or brightness, U, V = colors).
+->
+- equalizeHist
+->
+- Convert to BGR or RGB.
+"""
+import cv2
+from matplotlib import pyplot as plt
+
+# grayscale
+image = cv2.imread("../images/plane_256x256.jpg", cv2.IMREAD_GRAYSCALE)
+
+# Enhance image
+image_enhanced = cv2.equalizeHist(image)
+
+# Show image
+plt.imshow(image_enhanced, cmap="gray"), plt.axis("off")
+plt.show()
+
+# color
+image_bgr = cv2.imread("../images/plane.jpg")
+
+# Convert to YUV
+image_yuv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2YUV)
+
+# Apply histogram equalization
+image_yuv[:, :, 0] = cv2.equalizeHist(image_yuv[:, :, 0])
+
+# Convert to RGB
+image_rgb = cv2.cvtColor(image_yuv, cv2.COLOR_YUV2RGB)
+
+# Show image
+plt.imshow(image_rgb), plt.axis("off")
+plt.show()
---
+++ examples-08--image-processing/example-08-08--opencv--isolating-colors--cvtColor--inRange--bitwise_and.py
@@ -0,0 +1,81 @@
+"""
+Define a range of colors and then apply a mask to the image.
+->
+Convert an image into HSV (hue, saturation, and value).
+->
+Define a range of values we want to isolate.
+->
+Create a mask for the image.
+->
+Apply the mask to the image using "bitwise_and" and convert to a desired output format.
+"""
+# Load libraries
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load image
+image_bgr = cv2.imread('../images/plane_256x256.jpg')
+# array([[[212, 163, 107],
+#         [210, 161, 105],
+#         [209, 159, 106],
+#         ...,
+#         [208, 154,  93],
+#         [208, 154,  93],
+#         [208, 154,  93]],
+# ...
+
+# Convert BGR to HSV
+image_hsv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
+# array([[[104, 126, 212],
+#         [104, 128, 210],
+#         [105, 126, 209],
+#         ...,
+#         [104, 141, 208],
+#         [104, 141, 208],
+#         [104, 141, 208]],
+# ...
+
+# Define range of blue values in HSV
+lower_blue = np.array([50, 100, 50])
+upper_blue = np.array([130, 255, 255])
+
+# Create mask
+mask = cv2.inRange(image_hsv, lower_blue, upper_blue)
+# array([[255, 255, 255, ..., 255, 255, 255],
+#        [255, 255, 255, ..., 255, 255, 255],
+#        [255, 255, 255, ..., 255, 255, 255],
+#        ...,
+#        [255, 255, 255, ...,   0,   0,   0],
+#        [255, 255, 255, ...,   0,   0,   0],
+#        [255, 255, 255, ...,   0,   0,   0]], dtype=uint8)
+
+# Mask image
+image_bgr_masked = cv2.bitwise_and(image_bgr, image_bgr, mask=mask)
+# array([[[212, 163, 107],
+#         [210, 161, 105],
+#         [209, 159, 106],
+#         ...,
+#         [208, 154,  93],
+#         [208, 154,  93],
+#         [208, 154,  93]],
+# ...
+
+# Convert BGR to RGB
+image_rgb = cv2.cvtColor(image_bgr_masked, cv2.COLOR_BGR2RGB)
+# array([[[107, 163, 212],
+#         [105, 161, 210],
+#         [106, 159, 209],
+#         ...,
+#         [ 93, 154, 208],
+#         [ 93, 154, 208],
+#         [ 93, 154, 208]],
+# ...
+
+# Show image
+plt.imshow(image_rgb), plt.axis("off")
+plt.show()
+
+# Show image and keep only the white areas
+plt.imshow(mask, cmap='gray'), plt.axis("off")
+plt.show()
