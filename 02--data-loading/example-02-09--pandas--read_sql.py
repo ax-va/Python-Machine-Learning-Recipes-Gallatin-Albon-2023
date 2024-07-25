73b23ad9acc5c7e7169e245c8b9b795b17a59aba
ax-va <vasiliev.informatik@t-online.de>
2024-07-25 13:33:43 +0200

    update

--- 02--data-loading/example-02-09--pandas--read_sql.py
+++ 02--data-loading/example-02-09--pandas--read_sql.py
@@ -42,6 +42,6 @@ df.info()
 # dtypes: datetime64[ns](1), int64(2)
 # memory usage: 2.5 KB
 
-# Notice the difference between read_sql and read_sql_query:
+# Notice that the difference between read_sql and read_sql_query is as follows:
 # read_sql is a wrapper-function created for convenience that may call
 # either read_sql_query or read_sql_table depending on the input we provide.
