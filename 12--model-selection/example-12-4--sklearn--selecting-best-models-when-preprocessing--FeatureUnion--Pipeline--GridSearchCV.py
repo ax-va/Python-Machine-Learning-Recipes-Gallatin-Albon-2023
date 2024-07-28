4a5d8d25378fcd421d1a7b481cf9c28417e0e9c3
ax-va <vasiliev.informatik@t-online.de>
2024-07-28 12:04:03 +0200

    update

--- 12--model-selection/example-12-4--sklearn--selecting-best-models-when-preprocessing--FeatureUnion--Pipeline--GridSearchCV.py
+++ 12--model-selection/example-12-4--sklearn--selecting-best-models-when-preprocessing--FeatureUnion--Pipeline--GridSearchCV.py
@@ -3,6 +3,17 @@ Include a preprocessing step during model selection.
 ->
 Create a pipeline that includes the preprocessing step
 and any of its parameters to use in GridSearchCV.
+
+Notice that preprocessing should be built into the pipeline.
+
+Explanation:
+The general strategy is preprocessing only the training data and
+then applying that transformation to the test data.
+Thus, the test data know nothing about the training data.
+In the k-fold cross-validation, that can only be done correctly by using a pipeline.
+If we preprocess all the data before the k-fold cross-validation,
+the training and test data are not isolated from each other and have exchanged information.
+See also an experiment below.
 """
 import numpy as np
 from sklearn import datasets
@@ -64,4 +75,33 @@ best_model.best_estimator_
 #                                                 ('pca', PCA(n_components=1))])),
 #                 ('classifier',
 #                  LogisticRegression(C=7.742636826811269, max_iter=1000,
+#                                     penalty='l1', solver='liblinear'))])
+
+# best n_components
+best_model.best_estimator_.get_params()['preprocess__pca__n_components']
+# 1
+
+# # experiment: preprocessing is not built into the pipeline -> worse "best" model
+
+features_std = StandardScaler().fit_transform(features)
+features_pca = PCA(n_components=1).fit_transform(features_std)
+
+pipe_v2 = Pipeline(
+    [
+        ("classifier", LogisticRegression(max_iter=1000, solver='liblinear')),
+    ]
+)
+
+search_space_v2 = [
+    {
+        "classifier__penalty": ["l1", "l2"],
+        "classifier__C": np.logspace(0, 4, 10),
+    }
+]
+
+grid_search_v2 = GridSearchCV(pipe_v2, search_space_v2, cv=5, verbose=0, n_jobs=-1)
+best_model_v2 = grid_search_v2.fit(features_pca, target)
+best_model_v2.best_estimator_
+# Pipeline(steps=[('classifier',
+#                  LogisticRegression(C=2.7825594022071245, max_iter=1000,
 #                                     penalty='l1', solver='liblinear'))])
