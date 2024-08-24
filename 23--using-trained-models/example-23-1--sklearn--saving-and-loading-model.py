87071884d92184b61eb94ddb236c9b2c2adce1e4
ax-va <vasiliev.informatik@t-online.de>
2024-08-24 16:39:45 +0200

    update

---
+++ 23--using-trained-models/example-23-1--sklearn--saving-and-loading-model.py
@@ -0,0 +1,38 @@
+"""
+Save a trained Scikit-Learn and load it elsewhere.
+->
+Save the model as a pickle file using joblib.
+
+joblib extends pickle for cases with large NumPy arrays
+- a common occurrence for trained models in Scikit-Learn.
+"""
+import joblib
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+
+# Get scikit-learn version to know what version
+# of Scikit-Learn the model is serialized in.
+sklearn_version = sklearn.__version__.replace(".", "_")
+
+# Load data
+iris = datasets.load_iris()
+features = iris.data
+target = iris.target
+
+# Create decision tree classifier object
+classifier = RandomForestClassifier()
+# Train model
+model = classifier.fit(features, target)
+
+# Save model as pickle file
+joblib.dump(model, f"trained_models/model_{sklearn_version}.pkl")
+
+# Load model from file
+classifier = joblib.load(f"trained_models/model_{sklearn_version}.pkl")
+
+# Create new observation
+new_observation = [[5.2, 3.2, 1.1, 0.1]]
+# Predict observation's class
+prediction = classifier.predict(new_observation)
+# array([0])
